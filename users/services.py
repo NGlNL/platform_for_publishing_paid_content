9e6cbb0208a24bbb61182ae87389e35d0faa8032
Ilya Semak <ladysemak2001@gmail.com>
2025-03-29 12:28:39 +0300

    fix

--- users/services.py
+++ users/services.py
@@ -25,7 +25,7 @@ class StripeSessionView(View):
                 },
             ],
             mode="payment",
-            success_url="/users/success/",
-            cancel_url="users/cancel/",
+            success_url="http://158.160.141.206/users/success/",
+            cancel_url="http://158.160.141.206/users/cancel/",
         )
         return JsonResponse({"session_id": session.id})
